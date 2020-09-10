8938cebf30f27b1272c26fae52336526498576af
Invis1bleReborn <68089411+Invis1bleReborn@users.noreply.github.com>
2020-09-10 06:21:31 +0300

    Fix user table name.

--- src/Common/Shared/Infrastructure/DoctrineMigrations/Version20200910031724.php
+++ src/Common/Shared/Infrastructure/DoctrineMigrations/Version20200910031724.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200910030131 extends AbstractMigration
+final class Version20200910031724 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20200910030131 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE `user` (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid_binary)\', enabled TINYINT(1) NOT NULL, roles JSON NOT NULL, registered_by_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid_binary)\', date_registered DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', email VARCHAR(255) NOT NULL, password VARCHAR(128) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D64950F9BB84 (enabled), INDEX IDX_8D93D64927E92E18 (registered_by_id), INDEX IDX_8D93D64987CDE0C6 (date_registered), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid_binary)\', enabled TINYINT(1) NOT NULL, roles JSON NOT NULL, registered_by_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid_binary)\', date_registered DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', email VARCHAR(255) NOT NULL, password VARCHAR(128) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), INDEX IDX_1483A5E950F9BB84 (enabled), INDEX IDX_1483A5E927E92E18 (registered_by_id), INDEX IDX_1483A5E987CDE0C6 (date_registered), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE refresh_token (id INT AUTO_INCREMENT NOT NULL, refresh_token VARCHAR(128) NOT NULL, username VARCHAR(255) NOT NULL, valid DATETIME NOT NULL, UNIQUE INDEX UNIQ_C74F2195C74F2195 (refresh_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
@@ -28,6 +28,6 @@ final class Version20200910030131 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE refresh_token');
-        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE users');
     }
 }
