20848b54fe1e59fb6790c5ab0722131fcc27dfaf
Invis1bleReborn <68089411+Invis1bleReborn@users.noreply.github.com>
2020-09-14 02:14:01 +0300

    Add default roles.

--- src/IdentityAccess/Domain/Access/ValueObject/Role.php
+++ src/IdentityAccess/Domain/Access/ValueObject/Role.php
@@ -13,8 +13,10 @@ use MyCLabs\Enum\Enum;
  */
 class Role extends Enum
 {
-    private const USER = 'ROLE_USER';
-
     private const SUPERADMIN = 'ROLE_SUPER_ADMIN';
+
+    private const ALLOWED_TO_SWITCH = 'ROLE_ALLOWED_TO_SWITCH';
+
+    private const USER = 'ROLE_USER';
 
 }
