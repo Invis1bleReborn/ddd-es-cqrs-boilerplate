56fb222a98fca14e7a6c2ee58128f41008137b5c
Invis1bleReborn <68089411+Invis1bleReborn@users.noreply.github.com>
2020-09-16 12:12:44 +0300

    Add license block to files.

--- src/IdentityAccess/Ui/Identity/EnableUser/EnableUserRequestTransformerDecorator.php
+++ src/IdentityAccess/Ui/Identity/EnableUser/EnableUserRequestTransformerDecorator.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of invis1ble/ddd-es-cqrs-boilerplate.
+ *
+ * (c) Invis1ble <opensource.invis1ble@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace IdentityAccess\Ui\Identity\EnableUser;
 
 /**
