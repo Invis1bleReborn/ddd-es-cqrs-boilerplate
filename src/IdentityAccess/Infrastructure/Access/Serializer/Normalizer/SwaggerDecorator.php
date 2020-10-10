5d6bdac0b8466a2e8abd5755a5b9f6ee32bc1de9
Invis1bleReborn <68089411+Invis1bleReborn@users.noreply.github.com>
2020-10-11 01:06:11 +0300

    Improve JWT API documentation.

--- src/IdentityAccess/Infrastructure/Access/Serializer/Normalizer/SwaggerDecorator.php
+++ src/IdentityAccess/Infrastructure/Access/Serializer/Normalizer/SwaggerDecorator.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace IdentityAccess\Infrastructure\Access\Serializer\Normalizer;
 
 use ApiPlatform\Core\Documentation\Documentation;
+use IdentityAccess\Ui\Access\CreateToken\CreateTokenRequest;
+use IdentityAccess\Ui\Access\RefreshToken\RefreshTokenRequest;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
@@ -35,24 +37,74 @@ class SwaggerDecorator implements NormalizerInterface
     {
         $docs = $this->decorated->normalize($object, $format, $context);
 
-        $docs['paths'][$this->apiUriPrefix . '/tokens']['post']['responses'][201]['description'] = 'Token created.';
-        $docs['paths'][$this->apiUriPrefix . '/tokens']['post']['responses'][401] = [
-            'description' => 'Bad credentials or Account disabled.',
+        $patchData = [
+            CreateTokenRequest::class => [
+                'resource' => 'Token',
+                'uri' => '/tokens',
+                'responses' => [
+                    201 => [
+                        'description' => 'Token created.',
+                    ],
+                    401 => [
+                        'description' => 'Bad credentials or Account disabled.',
+                    ],
+                ],
+                'schema' => [
+                    'description' => 'User account credentials.',
+                    'name' => 'Token:Create',
+                ],
+            ],
+            RefreshTokenRequest::class => [
+                'resource' => 'Token',
+                'uri' => '/refresh_tokens',
+                'responses' => [
+                    201 => [
+                        'description' => 'Token refreshed.',
+                    ],
+                    401 => [
+                        'description' => 'Refresh token does not exist.',
+                    ],
+                ],
+                'schema' => [
+                    'description' => 'Refresh token.',
+                    'name' => 'Token:Refresh',
+                ],
+            ],
         ];
 
-        $docs['paths'][$this->apiUriPrefix . '/refresh_tokens']['post']['responses'][201]['description'] =
-            'Token refreshed.';
-        $docs['paths'][$this->apiUriPrefix . '/refresh_tokens']['post']['responses'][401] = [
-            'description' => 'Refresh token does not exist.',
-        ];
+        foreach ($patchData as $inputClass => $data) {
+            $uri = $this->apiUriPrefix . $data['uri'];
 
-        unset(
-            $docs['paths'][$this->apiUriPrefix . '/refresh_tokens']['post']['responses'][201]['links'],
-            $docs['paths'][$this->apiUriPrefix . '/refresh_tokens']['post']['responses'][404],
-            $docs['paths'][$this->apiUriPrefix . '/tokens']['post']['responses'][201]['links'],
-            $docs['paths'][$this->apiUriPrefix . '/tokens']['post']['responses'][404],
-            $docs['paths'][$this->apiUriPrefix . '/tokens/{id}']
-        );
+            foreach ($data['responses'] as $statusCode => $response) {
+                $docs['paths'][$uri]['post']['responses'][$statusCode]['description'] = $response['description'];
+            }
+
+            $schemaName = $data['resource'] . ':' . md5($inputClass);
+
+            $docs['components']['schemas'][$schemaName]['description'] =
+            $docs['components']['schemas'][$schemaName . ':jsonld']['description'] =
+            $docs['paths'][$uri]['post']['requestBody']['description'] = $data['schema']['description'];
+
+            foreach ($docs['paths'][$uri]['post']['requestBody']['content'] as $type => $content) {
+                $docs['paths'][$uri]['post']['requestBody']['content'][$type]['schema']['$ref'] = strtr(
+                    $content['schema']['$ref'],
+                    [$schemaName => $data['schema']['name']]
+                );
+            }
+
+            $docs['components']['schemas'][$data['schema']['name']] = $docs['components']['schemas'][$schemaName];
+            $docs['components']['schemas'][$data['schema']['name'] . ':jsonld'] =
+                $docs['components']['schemas'][$schemaName . ':jsonld'];
+
+            unset(
+                $docs['paths'][$uri]['post']['responses'][201]['links'],
+                $docs['paths'][$uri]['post']['responses'][404],
+                $docs['components']['schemas'][$schemaName],
+                $docs['components']['schemas'][$schemaName . ':jsonld']
+            );
+        }
+
+        unset($docs['paths'][$this->apiUriPrefix . '/tokens/{id}']);
 
         return $docs;
     }
