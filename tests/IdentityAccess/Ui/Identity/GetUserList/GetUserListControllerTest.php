04aae5bb23ceb79b64e64734b604c9c683f09f03
Invis1bleReborn <68089411+Invis1bleReborn@users.noreply.github.com>
2020-10-27 23:20:17 +0300

    Add GetUserList controller test.

---
+++ tests/IdentityAccess/Ui/Identity/GetUserList/GetUserListControllerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of invis1ble/ddd-es-cqrs-boilerplate.
+ *
+ * (c) Invis1ble <opensource.invis1ble@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace IdentityAccess\Ui\Identity\GetUserList;
+
+use IdentityAccess\Ui\UiTestCase;
+
+/**
+ * Class GetUserListControllerTest.
+ */
+class GetUserListControllerTest extends UiTestCase
+{
+    public function testGetUserList(): void
+    {
+        $client = $this->createClient();
+
+        $response = $this->getResource($client, '/users');
+
+        $this->assertAuthenticationRequired($response);
+
+        $rootUsername = 'root@acme.com';
+        $rootPassword = '111111';
+
+        $this->registerRootUser(null, $rootUsername, $rootPassword);
+        $this->authenticateClient($client, $rootUsername, $rootPassword);
+
+        $response = $this->getResource($client, '/users');
+
+        $this->assertOk($response);
+
+        $this->assertListItemMatchesJsonSchema([
+            'type' => 'object',
+            'properties' => [
+                'id' => ['type' => 'string'],
+                'email' => ['type' => 'email'],
+                'roles' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'string',
+                        'pattern' => '^ROLE_[A-Z_]+$',
+                    ],
+                ],
+                'enabled' => ['type' => 'boolean'],
+                'dateRegistered' => ['type' => 'string'],
+            ],
+            'additionalProperties' => true,
+            'required' => ['id', 'email', 'roles', 'enabled', 'dateRegistered'],
+        ], '/users', [
+            '_order[email]' => 'email',
+            '_order[enabled]' => 'enabled',
+            '_order[dateRegistered]' => 'dateRegistered',
+            '_properties[]' => null,
+            'email' => 'email',
+            'enabled' => 'enabled',
+            'dateRegistered[before]' => 'dateRegistered',
+            'dateRegistered[strictly_before]' => 'dateRegistered',
+            'dateRegistered[after]' => 'dateRegistered',
+            'dateRegistered[strictly_after]' => 'dateRegistered',
+        ]);
+    }
+
+    protected function setUp(): void
+    {
+        $this->setUpStorage();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->tearDownStorage();
+    }
+}
